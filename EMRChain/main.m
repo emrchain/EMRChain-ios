0a6ab11d56ff1210847ceca1ab4931b8966076bf
Scott Hermes <shermes@solstice-consulting.com>
2015-11-07 04:44:22 -0600

    Initial Commit

---
+++ EMRChain/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  EMRChain
+//
+//  Created by Scott Hermes on 11/7/15.
+//  Copyright © 2015 Solstice. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
